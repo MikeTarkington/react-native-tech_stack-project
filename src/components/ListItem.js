750baa76720d09a78cb061d337102fdb5df6b546
Mike Tarkington <tarkmike@gmail.com>
2017-07-09 15:18:16 -0700

    complete the tutorial

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+// include action creators
+import * as actions from '../actions';
 
 class ListItem extends Component {
+  // componentWillUpdate fires whenever the component is about to re-render
+  componentWillUpdate() {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
   render () {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+          onPress={() => this.props.selectLibrary(id)}
+        >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -20,4 +51,14 @@ const styles = {
   }
 }
 
-export default ListItem;
+// ownProps gives access to component props ie this.props.blah
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+// without mapStateToProps need to pass in null as first argument
+// second argument of actions makes the action creator automatically dispatch the action to the redux store
+// the connect argument of actions ensure that actions are also passed as props along with the ListItem component
+export default connect(mapStateToProps, actions)(ListItem);
