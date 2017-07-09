3bc211ad6a2f3a564af551f53ae38010d48d5049
Mike Tarkington <tarkmike@gmail.com>
2017-07-08 21:23:36 -0700

    learning redux intro and list view

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from  './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
     // provider can only have one direct child but its first child can have more
     <Provider store={createStore(reducers)}>
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText="Tech Stack" />
+        <LibraryList></LibraryList>
       </View>
     </Provider>
   );
